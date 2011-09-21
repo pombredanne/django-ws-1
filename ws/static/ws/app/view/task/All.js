641b352ea394d87ffb17f0f5fbe8c83e11fe34be
Ales Zabala Alava (Shagi) <shagi@gisa-elkartea.org>
2011-09-21 13:16:49 +0200

    Fix task grid actin column.
    - Move grid definition to initComponent
    - Renamed View action to Activate
    - Create Complete action
    - Add action icons

--- ws/static/ws/app/view/task/All.js
+++ ws/static/ws/app/view/task/All.js
@@ -4,31 +4,44 @@ Ext.define('WS.view.task.All', {
     autoScroll: true,
     title: 'Tasks',
     refreshable: true,
-    items:[{
-        xtype: 'grid',
-        store: 'Tasks',
-        columns: [
-            {header: 'Task',  dataIndex: 'task',  flex: 1},
-            {header: 'User',  dataIndex: 'user',  flex: 1},
-            {header: 'Process', dataIndex: 'process', flex: 1},
-            {header: 'Process type', dataIndex: 'process_type', flex: 1},
-            {header: 'Priority', dataIndex: 'priority', flex: 1},
-            {header: 'Date', dataIndex: 'date', flex: 1},
-            {header: 'Status', dataIndex: 'status', flex: 1, renderer: this.statusRenderer},
-            {xtype:'actioncolumn', 
-                width: 70,
-                items: [{
-                    icon: '/static/ws/images/edit.png',  // Use a URL in the icon config
-                    tooltip: 'View',
-                    handler: this.viewTask
-                }]
-            },
-        ],
-    }],
 
-    viewTask: function(grid, rowIndex, colIndex) {
+    initComponent: function() {
+        this.items = [{
+            xtype: 'grid',
+            store: 'Tasks',
+            columns: [
+                {header: 'Task',  dataIndex: 'task',  flex: 1},
+                {header: 'User',  dataIndex: 'user',  flex: 1},
+                {header: 'Process', dataIndex: 'process', flex: 1},
+                {header: 'Process type', dataIndex: 'process_type', flex: 1},
+                {header: 'Priority', dataIndex: 'priority', flex: 1},
+                {header: 'Date', dataIndex: 'date', flex: 1},
+                {header: 'Status', dataIndex: 'status', flex: 1, renderer: this.statusRenderer},
+                {xtype:'actioncolumn', 
+                    width: 70,
+                    items: [{
+                        icon: '/static/ws/images/activate.png',  // Use a URL in the icon config
+                        tooltip: 'Activate',
+                        handler: this.activateTask
+                    },{
+                        icon: '/static/ws/images/accept.png',  // Use a URL in the icon config
+                        tooltip: 'Complete',
+                        handler: this.completeTask
+                    }]
+                },
+            ],
+        }];
+        this.callParent(arguments);
+    },
+
+    activateTask: function(grid, rowIndex, colIndex) {
         var record = grid.getStore().getAt(rowIndex);
-        alert("View " + record.get('taskid')+' '+record.get('task'));
+        alert("Activate " + record.get('taskid')+' '+record.get('task'));
+    },
+
+    completeTask: function(grid, rowIndex, colIndex) {
+        var record = grid.getStore().getAt(rowIndex);
+        alert("Complete " + record.get('taskid')+' '+record.get('task'));
     },
 
     statusRenderer: function(value, metadata, record, rowIndex, colIndex, store, view) {
