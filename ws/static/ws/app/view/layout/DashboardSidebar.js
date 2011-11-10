97fa037023cac16a6052a425fea514930632e792
Ales Zabala Alava (Shagi) <shagi@gisa-elkartea.org>
2011-11-10 19:02:11 +0000

    group dashboard sidebar's buttons in a fieldset

--- ws/static/ws/app/view/layout/DashboardSidebar.js
+++ ws/static/ws/app/view/layout/DashboardSidebar.js
@@ -2,21 +2,25 @@ Ext.define('WS.view.layout.DashboardSidebar', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.dashboardsidebar',
     items: [{
-        xtype: 'button',
-        text: 'Tasks',
-        action: 'taskPortlet',
-    },{
-        xtype: 'button',
-        text: 'My tasks',
-        action: 'myTasks'
-    },{
-        xtype: 'button',
-        text: 'Process Starter',
-        action: 'startProcess',
-    },{
-        xtype: 'button',
-        text: 'Processes',
-        action: 'processes',
+        xtype: 'fieldset',
+        title: 'Add portlets',
+        items: [{
+            xtype: 'button',
+            text: 'Tasks',
+            action: 'taskPortlet',
+        },{
+            xtype: 'button',
+            text: 'My tasks',
+            action: 'myTasks'
+        },{
+            xtype: 'button',
+            text: 'Process Starter',
+            action: 'startProcess',
+        },{
+            xtype: 'button',
+            text: 'Processes',
+            action: 'processes',
+        }],
     }],
 });
 
