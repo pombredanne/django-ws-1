724ee67935b4414cd5aba2c9fd825e1d4f25bb92
Ales Zabala Alava (Shagi) <shagi@gisa-elkartea.org>
2011-11-15 22:01:14 +0000

    modeladmins for node and processes

--- ws/admin.py
+++ ws/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 from ws.models import *
 
+class NodeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'role', 'workflow', 'join', 'split',
+                    'task_name', 'params']
+    list_filter = ['workflow', 'role']
+
 class TransitionAdmin(admin.ModelAdmin):
     list_display = ['parent', 'child', 'condition', 'workflow']
     list_filter = ['parent', 'child', 'workflow']
 
+class ProcessAdmin(admin.ModelAdmin):
+    list_display = ['__unicode__', 'workflow', 'start_date', 'end_date']
+    list_filter = ['workflow']
+
+
 admin.site.register(Workflow)
-admin.site.register(Node)
+admin.site.register(Node, NodeAdmin)
 admin.site.register(Transition, TransitionAdmin)
-admin.site.register(Process)
+admin.site.register(Process, ProcessAdmin)
 admin.site.register(Task)
