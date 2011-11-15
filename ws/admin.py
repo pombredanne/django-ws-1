8c39ac62e837379db30d0b048423c518347659bd
Ales Zabala Alava (Shagi) <shagi@gisa-elkartea.org>
2011-11-15 18:58:21 +0000

    Transition admin

--- ws/admin.py
+++ ws/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from ws.models import *
 
+class TransitionAdmin(admin.ModelAdmin):
+    list_display = ['parent', 'child', 'condition', 'workflow']
+    list_filter = ['parent', 'child', 'workflow']
+
 admin.site.register(Workflow)
 admin.site.register(Node)
-admin.site.register(Transition)
+admin.site.register(Transition, TransitionAdmin)
 admin.site.register(Process)
 admin.site.register(Task)
