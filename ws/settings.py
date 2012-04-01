b05f49dcdd16408d327ae62934bbb54ab3a17d12
Unai Zalakain <unai@gisa-elkartea.org>
2012-04-02 00:59:25 +0200

    WS relevant settings in separate module. So for installing WSBPMS it's needed to:
    from ws.settings import *
    
    INSTALLED_APPS = (
        ...
        'ws',
        'djcelery',
        'guardian',
        )

---
+++ ws/settings.py
@@ -0,0 +1,20 @@
+AUTHENTICATION_BACKENDS = (
+        'django.contrib.auth.backends.ModelBackend',
+        'guardian.backends.ObjectPermissionBackend',
+        )
+
+LOGIN_URL = '/ws/login'
+ANONYMOUS_USER_ID = 1
+
+import djcelery
+djcelery.setup_loader()
+BROKER_HOST = "localhost"
+BROKER_PORT = 5672
+BROKER_USER = "guest"
+BROKER_PASSWORD = "guest"
+BROKER_VHOST = "/"
+
+TEST_RUNNER = 'djcelery.contrib.test_runner.CeleryTestSuiteRunner'
+CELERY_IMPORTS = 'ws.tasks', 'ws.celery.bpm'
+CELERY_RESULT_BACKEND = 'amqp'
+CELERYD_HIJACK_ROOT_LOGGER = False
