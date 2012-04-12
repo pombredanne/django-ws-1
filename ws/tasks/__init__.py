17b0cf3ea52f80c8109c7e3c5d59c9563847192f
Ales Zabala Alava (Shagi) <shagi@gisa-elkartea.org>
2012-04-12 22:45:30 +0200

    simplify task loading

--- ws/tasks/__init__.py
+++ ws/tasks/__init__.py
@@ -1,8 +1,3 @@
-__all__ = [ 'http', 'bpm', 'dummy']
-
-
-from celery.loaders import current_loader
-current_loader = current_loader()
-
-for module in __all__:
-    current_loader.import_module('{0}.{1}'.format(__name__, module))
+from ws.tasks.bpm import subprocess
+from ws.tasks.dummy import dummy, endless, add
+from ws.tasks.http import download
