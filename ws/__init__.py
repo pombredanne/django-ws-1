037f0f06ed3643f4b6bdcd562298138c3866fbd1
Unai Zalakain <unai@gisa-elkartea.org>
2012-04-15 21:06:03 +0200

    Check if a task is a

--- ws/__init__.py
+++ ws/__init__.py
@@ -3,13 +3,15 @@ from __future__ import absolute_import
 from celery.states import READY_STATES
 from celery.registry import tasks as task_registry
 
+
 STATES = dict([(s,s) for s in ('PENDING', 'RETRY', 'STARTED') + tuple(READY_STATES) ])
 
 CONDITIONS = dict([(s,s) for s in ('XOR', 'AND')])
 
 def get_registered_tasks():
     from celery.loaders import current_loader
+    from ws.celery.tasks import BPMTask
     current_loader = current_loader()
     current_loader.import_default_modules()
     return [ key for key, value in task_registry.items()\
-            if hasattr(value, 'form') ]
+            if isinstance(value, BPMTask) ]
