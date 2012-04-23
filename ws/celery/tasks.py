2b08fa563950b92649b6b479763844ec2dad14d7
Unai Zalakain <unai@gisa-elkartea.org>
2012-04-23 22:51:04 +0200

    Define 'on_start' and 'on_revoke' functions for tasks. djcelery defines 'on_success', 'on_retry' and 'on_failure'.

--- ws/celery/tasks.py
+++ ws/celery/tasks.py
@@ -16,10 +16,10 @@ class BPMTask(AbortableTask):
         send_task('ws.celery.bpm.task_progress', kwargs={
             'pk': workflow_task, 'progress': progress})
 
-    def iter_progress(self, process, every):
+    def iter_progress(self, process, every, regexp='(\d{1,3})%'):
         while not process.terminated:
             try:
-                process.expect('(\d{1,3})%')
+                process.expect(regexp)
             except EOF:
                 break
             progress = process.match.groups()[0]
@@ -32,3 +32,18 @@ class BPMTask(AbortableTask):
                 return process
             else:
                 self.notify_progress(workflow_task, progress)
+
+    def on_start(self, task_id, args, kwargs):
+        pass
+
+    def on_success(self, retval, task_id, args, kwargs):
+        pass
+
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        pass
+
+    def on_retry(self, exc, task_id, args, kwargs, einfo):
+        pass
+
+    def on_revoke(self, task_id, args, kwargs):
+        pass
