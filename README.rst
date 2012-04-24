8dcb5888c84a530f85d87ada54bca414404625bd
Ales Zabala Alava (Shagi) <shagi@gisa-elkartea.org>
2012-04-24 12:11:17 +0200

    Apply AGPL license

---
+++ README.rst
@@ -0,0 +1,17 @@
+====================
+Django WorkSheet BPM
+====================
+
+A Business Process Manager for Django. Uses Celery for task automation and
+ExtJS for web interface.
+
+
+License
+=======
+
+django-ws is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+See LICENSE file for full license details.
--- setup.py
+++ setup.py
@@ -4,17 +4,26 @@ import sys, os
 
 version = '0.1'
 
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Other Environment',
+    'Framework :: Django',
+    'Programming Language :: Python',
+    'License :: OSI Approved :: GNU Affero General Public License v3',
+]
+
+long_description = file('README.rst').read()
+
 setup(name='django-%s' % APP_NAME,
       version=version,
       description="WS BPMS",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      long_description=long_description,
+      classifiers=classifiers, # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
-      author='Ales Zabala Alava (Shagi)',
-      author_email='shagi@gisa-elkartea.org',
-      url='http://lagunak.gisa-elkartea.org/projects/wsbpms',
-      license='GPL',
+      author='GISA Elkartea',
+      author_email='kontaktua@gisa-elkartea.org',
+      url='http://lagunak.gisa-elkartea.org/projects/django-ws',
+      license='AGPL',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       package_dir={
           APP_NAME: APP_NAME,
