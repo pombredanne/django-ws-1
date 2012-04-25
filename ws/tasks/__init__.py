4a307017c474596decbf2ecc4bd0c523e091c01c
Unai Zalakain <unai@gisa-elkartea.org>
2012-04-26 01:31:42 +0200

    Move ws.celery.tasks to ws.tasks, so ws.celery.tasks.BPMTask to ws.tasks.BPMTask

--- ws/tasks/__init__.py
+++ ws/tasks/__init__.py
@@ -17,6 +17,58 @@
 #  along with django-ws. If not, see <http://www.gnu.org/licenses/>.             #
 ##################################################################################
 
+####################
 from ws.tasks.bpm import subprocess
 from ws.tasks.dummy import dummy, endless, add
 from ws.tasks.http import download
+####################
+
+
+from time import sleep
+
+from celery.contrib.abortable import AbortableTask
+from celery.execute import send_task
+from pexpect import EOF
+
+from ws.forms import BPMTaskForm
+
+
+class BPMTask(AbortableTask):
+    abstract = True
+    form = BPMTaskForm
+
+    def notify_progress(self, workflow_task, progress):
+        send_task('ws.celery.bpm.task_progress', kwargs={
+            'pk': workflow_task, 'progress': progress})
+
+    def iter_progress(self, process, every, regexp='(\d{1,3})%'):
+        while not process.terminated:
+            try:
+                process.expect(regexp)
+            except EOF:
+                break
+            progress = process.match.groups()[0]
+            yield progress
+            sleep(every)
+
+    def track_task(self, process, workflow_task, every=30):
+        for progress in self.iter_progress(process, every):
+            if self.is_aborted():
+                return process
+            else:
+                self.notify_progress(workflow_task, progress)
+
+    def on_start(self, task_id, args, kwargs):
+        pass
+
+    def on_success(self, retval, task_id, args, kwargs):
+        pass
+
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        pass
+
+    def on_retry(self, exc, task_id, args, kwargs, einfo):
+        pass
+
+    def on_revoke(self, task_id, args, kwargs):
+        pass
