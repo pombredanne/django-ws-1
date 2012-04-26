4411c0283c83e589b2a75e84a62c2aee89042467
Unai Zalakain <unai@gisa-elkartea.org>
2012-04-26 13:15:20 +0200

    License header shortened

--- headache.sh
+++ headache.sh
@@ -1,1 +1,1 @@
-headache -c headacheconfig -h headacheheader *py ws/*py ws/celery/* ws/tasks/* ws/tests/*
+headache -c headacheconfig -h headacheheader *py ws/*py ws/celery/*py ws/tasks/*py ws/tests/*py
