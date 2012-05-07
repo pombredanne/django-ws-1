6d1dff7b127cc0bc09368d28af82a4b9010d2b94
Unai Zalakain <unai@gisa-elkartea.org>
2012-05-07 02:49:47 +0200

    tasks intended for humans to complete

---
+++ ws/tasks/human.py
@@ -0,0 +1,37 @@
+from django.utils.translation import ugettext as _
+
+from ws.tasks import BPMTask
+from ws import forms
+
+
+class QuestionForm(forms.BPMForm):
+    title = forms.CharField(max_length=100, label=_('Title'))
+    description = forms.CharField(max_length=1000, label=_('Description'),
+            required=False)
+    answer = forms.CharField(label=_('Answer'))
+
+    @forms.with_cleaned_data
+    def get_title(self):
+        return self.cleaned_data['title']
+
+    @forms.with_cleaned_data
+    def get_description(self):
+        return self.cleaned_data['description']
+
+
+class question(BPMTask):
+    form = QuestionForm
+
+    def call(self, title, description, answer):
+        return answer
+
+
+class BooleanQuestionForm(QuestionForm):
+    answer = forms.BooleanField(label=_('Answer'), required=False)
+
+
+class boolean_question(BPMTask):
+    form = BooleanQuestionForm
+
+    def call(self, title, description, answer):
+        return answer
